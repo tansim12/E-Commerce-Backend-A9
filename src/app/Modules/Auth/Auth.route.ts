2a2fcccf0565cfb12fcf2fc9faf309f66f8765a4
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-12-02 12:50:49 +0600

    auth zod validation

--- src/app/Modules/Auth/Auth.route.ts
+++ src/app/Modules/Auth/Auth.route.ts
@@ -3,12 +3,21 @@ import { AuthController } from "./Auth.controller";
 
 import { UserRole } from "@prisma/client";
 import { authMiddleWare } from "../../middleware/authMiddleware";
-
+import validationMiddleWare from "../../middleware/validationMiddleWare";
+import { authSchemas } from "./Auth.zodValidation";
 
 const router = express.Router();
 
-router.post("/signup", AuthController.signUp);
-router.post("/login", AuthController.loginUser);
+router.post(
+  "/signup",
+  validationMiddleWare(authSchemas.signupSchema),
+  AuthController.signUp
+);
+router.post(
+  "/login",
+  validationMiddleWare(authSchemas.loginSchema),
+  AuthController.loginUser
+);
 
 router.post("/refresh-token", AuthController.refreshToken);
 router.post(
